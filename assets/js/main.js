5ab7d609067246ecc24bbd01dd6e417f43f42b33
zamariolajr <zamariolajr@gmail.com>
2022-11-06 10:10:13 -0300

    Consumo da api e uso de promise com fetch e debugger no fonte.

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,13 @@
-console.log('Sucesso!!!');
+const offset = 0;
+const limit = 10;
+
+const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`;
+
+fetch(url)
+    .then((response) => response.json())            //convertendo resposta do body em json
+    .then((jsonBody) => jsonBody.results)
+    .then((pokemonList) => {
+        debugger;
+        console.log(pokemonList)})      //printando body json
+    .catch((error) => console.error(error))         //tratamento de erro
+    
