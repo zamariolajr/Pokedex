0015c0425e83eaca18afd43c177ae3e5abaa186d
zamariolajr <zamariolajr@gmail.com>
2022-11-10 21:40:41 -0300

    adicionando multiplas requisições a api.

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,18 +1,20 @@
 
+function convertPokemonTypesToLi(pokemonTypes) {
+    return pokemonTypes.map((typeSlot) => `<li class="type">${typeSlot.type.name}</li>`)
+}
 
 function convertPokemonToLi(pokemon) {
     return `
         <li class="pokemon">
-            <span class="number">#003</span>
+            <span class="number">${pokemon.order}</span>
             <span class="name">${pokemon.name}</span>
     
             <div class="detail">
                 <ol class="types">
-                    <li class="type"></li>
-                    <li class="type"></li>
+                    ${convertPokemonTypesToLi(pokemon.types).join('')}
                 </ol>
     
-                <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/3.svg"
+                <img src="${pokemon.sprites.other.dream_world.front_default}"
                     alt="${pokemon.name}">
             </div>
 
--- assets/js/poke-api.js
+++ assets/js/poke-api.js
@@ -1,10 +1,19 @@
 const pokeApi = {}
 
-pokeApi.getPokemons = (offset = 0, limit = 10) => {
-    
-    const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`;
-    return fetch(url)
-    .then((response) => response.json())
-    .then((jsonBody) => jsonBody.results)
-    .catch((error) => console.error(error))
+pokeApi.getPokemonDetail = (pokemon) => {
+    return fetch(pokemon.url).then((response) => response.json())
 }
+
+pokeApi.getPokemons = (offset = 0, limit = 5) => {
+    
+    const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`
+    
+    return fetch(url)
+        .then((response) => response.json())
+        .then((jsonBody) => jsonBody.results)
+        .then((pokemons) => pokemons.map(pokeApi.getPokemonDetail))
+        .then((detailRequests) => Promise.all(detailRequests))
+        .then((pokemonsDetails) => pokemonsDetails)
+        .catch((error) => console.error(error))
+}
+
