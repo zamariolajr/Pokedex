d0907f41d247bc114d53ab2a7201be62c174aecf
zamariolajr <zamariolajr@gmail.com>
2022-11-12 14:42:16 -0300

    Definindo objeto modelo e carregando dados da api de maneira dinamica

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,28 +1,25 @@
-
-function convertPokemonTypesToLi(pokemonTypes) {
-    return pokemonTypes.map((typeSlot) => `<li class="type">${typeSlot.type.name}</li>`)
-}
+const pokemonList = document.getElementById('pokemonList')
 
 function convertPokemonToLi(pokemon) {
     return `
-        <li class="pokemon">
-            <span class="number">${pokemon.order}</span>
+        <li class="pokemon ${pokemon.type}">
+            <span class="number">${pokemon.number}</span>
             <span class="name">${pokemon.name}</span>
     
             <div class="detail">
                 <ol class="types">
-                    ${convertPokemonTypesToLi(pokemon.types).join('')}
+                    ${pokemon.types.map((type) => `<li class="type ${type}">${type}</li>`).join('')}
                 </ol>
     
-                <img src="${pokemon.sprites.other.dream_world.front_default}"
-                    alt="${pokemon.name}">
+                <img src="${pokemon.photo}"
+                     alt="${pokemon.name}">
             </div>
 
         </li>
     `
 }
 
-const pokemonList = document.getElementById('pokemonList')
+
 
 pokeApi.getPokemons().then((pokemons = []) => {
 
